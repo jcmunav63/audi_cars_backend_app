2b4700b5b994e69ae1bf7ba6ae5e4c762ceb86bf
jcmunav63 <jcmunav63@gmail.com>
2024-05-15 14:25:35 -0600

    Edit User model, users_controller, and routes file to display API endpoint of users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+      end
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
