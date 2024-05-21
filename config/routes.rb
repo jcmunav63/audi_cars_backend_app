c17961f75c5557cb8fcfa0dd7d4caba2b0546e8d
jcmunav63 <jcmunav63@gmail.com>
2024-05-20 22:44:22 -0600

    Add Welcome page & controller; add production build file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -14,5 +15,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "welcome#index"
 end
