75b56ffb0c342d38a9e9eeba4c420b4529a1d94b
jcmunav63 <jcmunav63@gmail.com>
2024-05-21 21:48:41 -0600

    Create migrations for Users table. Edit User controller and model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_184806) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_002710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
