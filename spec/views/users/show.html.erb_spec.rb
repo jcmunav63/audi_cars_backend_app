45fd5588bad9a5ebb837ef87f5f273b16651be11
jcmunav63 <jcmunav63@gmail.com>
2024-05-22 09:34:15 -0600

    Edit files to correct linters errors.

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "users/show", type: :view do
+RSpec.describe 'users/show', type: :view do
   before(:each) do
     assign(:user, User.create!(
-      email: "Email",
-      name: "Name",
-      password: "Password"
-    ))
+                    email: 'Email',
+                    name: 'Name',
+                    password: 'Password'
+                  ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Email/)
     expect(rendered).to match(/Name/)
