45ade455c3ad12fe34fbadc32949fe13a51ff81c
jcmunav63 <jcmunav63@gmail.com>
2024-05-30 11:36:56 -0600

    Edit puma file.

--- config/puma.rb
+++ config/puma.rb
@@ -11,6 +11,12 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
