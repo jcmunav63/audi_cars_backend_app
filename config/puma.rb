29163858ef70f8b72b1c7155c6733316e35ec4ad
jcmunav63 <jcmunav63@gmail.com>
2024-05-30 18:06:39 -0600

    Edit puma config file.

--- config/puma.rb
+++ config/puma.rb
@@ -21,6 +21,7 @@ if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
+  port ENV.fetch("PORT") { 10000 }
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
